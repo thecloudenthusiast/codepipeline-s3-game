5562457b7399642a651714f8db5479764546da74
The Cloud Enthusiast <152938496+thecloudenthusiast@users.noreply.github.com>
2023-12-09 19:14:35 -0500

    Update script.js
    script edited to reflect the new images

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card6', img: 'images/akinpawpaw.jpg' },
-        { name: 'card6', img: 'images/akinpawpaw.jpg' },
+        { name: 'card1', img: 'images/airport.jpg' },
+        { name: 'card1', img: 'images/airport.jpg' },
+        { name: 'card2', img: 'images/buffet.jpg' },
+        { name: 'card2', img: 'images/buffet.jpg' },
+        { name: 'card3', img: 'images/catchme.jpg' },
+        { name: 'card3', img: 'images/catchme.jpg' },
+        { name: 'card4', img: 'images/kenneth.jpg' },
+        { name: 'card4', img: 'images/kenneth.jpg' },
+        { name: 'card5', img: 'images/mickey.jpg' },
+        { name: 'card5', img: 'images/mickey.jpg' },
+        { name: 'card6', img: 'images/smile.jpg' },
+        { name: 'card6', img: 'images/smile.jpg' },
         // ...add more pairs as needed
     ];
 
